b8a5dc551971b02f133ccc17d6433c1e4a1e9ef4
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-02-18 16:41:53 +0000

    basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+//  Copyright (2025) Aaron Rivelino All rights reserved.
+// Created By : Aaron Rivelino
+// ate : Feb 15, 2025
+// The code does simple math calculations
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "3+3=" << (3+3) << std::endl;
+    std::cout << "12-3=" << (12-3) << std::endl;
+    std::cout << "15x8=" << (15*8) << std::endl;
+    std::cout << "11/4=" << (11/4) << std::endl;
+    std::cout << "2^7=" << (2^7) << std::endl;
+    std::cout << "square root of 80 =" << sqrt(80) << std::endl;
+}
